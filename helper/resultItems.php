62c3db5fbb54678c4deedfd363235a6a656f1590
Samuel Szoniecky <samszon@gmail.com>
2021-06-10 06:11:26 +0200

    v 0.07

---
+++ helper/resultItems.php
@@ -0,0 +1,31 @@
+<?php
+namespace OmekaTheme\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+class resultItems extends AbstractHelper
+{
+
+    /**
+     * Récupère les items correspondant aux résultats de process
+     *
+     * @param array    $docs   liste des docs
+     * 
+     * @return array
+     */
+    public function __invoke($docs)
+    {
+        $this->api = $this->getView()->api();
+        $result = ['items'=>[],'medias'=>[]];
+        foreach ($docs as $d) {
+            $oItem = $this->api->read('items', $d)->getContent();
+            $result['items'][]=$oItem;
+            $result['medias']=array_merge($result['medias'], $oItem->media());
+        }
+
+        return $result;
+
+    }
+
+
+}
